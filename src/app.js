0dccb7899a7d17e0815e35d2f401bb49b8753523
Jdalyjulian <121656174+Jdalyjulian@users.noreply.github.com>
2023-02-21 16:48:18 -0600

    Set main icon attribution

--- src/app.js
+++ src/app.js
@@ -29,12 +29,18 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/04d@2x.png`
+  );
 }
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";

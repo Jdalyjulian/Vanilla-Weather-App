225ff0420508518ddc204611e542b97d4b7ba17b
Jdalyjulian <121656174+Jdalyjulian@users.noreply.github.com>
2023-02-21 12:09:44 -0600

    API key added, top/main elements updated with JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-let apiKey = "694c43be0cabd6cfcf95f92c1eb26fbd";
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Austin&units=metric&appid=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);

10c489396066f2e457490d08a57109b6141e656b
Jdalyjulian <121656174+Jdalyjulian@users.noreply.github.com>
2023-02-25 20:49:56 -0600

    moved forecast HTML into JS

--- src/app.js
+++ src/app.js
@@ -22,6 +22,30 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thurs", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+     <div class="col-2">
+       <div class="forecast-date">${day}</div>
+       <img src="https://ssl.gstatic.com/onebox/weather/48/cloudy.png" alt="" />
+       <div class="forecast-temperature">
+         <span class="forecast-temperature-max"> 18° </span>
+         <span class="forecast-temperature-min"> 12° </span>
+       </div>
+     </div>
+   
+   `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -88,3 +112,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Austin");
+displayForecast();
